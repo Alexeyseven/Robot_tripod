50e92d750d9c91f71c4bc7788a35ba3da9ca9363
Alexeyseven <alexeyseven77@gmail.com>
2023-11-14 14:15:06 +0300

    auto mode

--- tripod.py
+++ tripod.py
@@ -12,72 +12,65 @@ s.listen(4)
 os.system('start cmd /k python robot.py')
 conn, addr = s.accept()
 
-mes =[b'q', b'e', b'y']
-
-
-def send(mes):
-    delay = 1/velocity.get()
-    conn.send(mes[0])
-    time.sleep(delay)
-    conn.send(mes[1])
-    time.sleep(delay)
-    conn.send(mes[2])
-    time.sleep(delay)
-
-
-def down():
-    global mes
-    mes = [b'w', b't', b'i']
-
-
-def up():
-    global mes
-    mes = [b'q', b'e', b'y']
-
-
-def left():
-    global mes
-    mes = [b'q', b't', b'i']
-
-
-def right():
-    global mes
-    mes = [b'w', b'e', b'y']
-
-
-def close():
-    global mes
-    mes = [b'', b'e', b'i']
-
-
-def far():
-    global mes
-    mes = [b'', b't', b'y']
+stack = []
 
 
 def run():
-    conn.send(b'r2')
-    print(conn.recv(50))
-    conn.send(b'f2')
-    print(conn.recv(50))
-    conn.send(b'd2')
-    print(conn.recv(50))
-    conn.send(b'u2')
-    print(conn.recv(50))
-    conn.send(b'l2')
-    print(conn.recv(50))
-    conn.send(b'c2')
-    print(conn.recv(50))
-    conn.send(b'd2')
-    print(conn.recv(50))
-    conn.send(b'u2')
-    print(conn.recv(50))
+    while not keyboard.is_pressed('q'):
+        for i in stack:
+            print(stack)
+            conn.send(i)
+            print(conn.recv(50))
+
+
+def up():
+    global stack
+    stack.append(str.encode('u' + str(value_up.get())))
+
+
+def down():
+    global stack
+    stack.append(str.encode('d' + str(value_down.get())))
+
+
+def right():
+    global stack
+    stack.append(str.encode('r' + str(value_right.get())))
+
+
+def left():
+    global stack
+    stack.append(str.encode('l' + str(value_left.get())))
+
+
+def close():
+    global stack
+    stack.append(str.encode('c' + str(value_close.get())))
+
+
+def far():
+    global stack
+    stack.append(str.encode('f' + str(value_far.get())))
+
+
+def prog():
+    Label(root, text=stack).place(x=200, y=140)
+
+
+def clr():
+    global stack
+    stack = []    
+
 
 root = Tk()
-root.geometry('400x250')
+root.geometry('400x437')
 
-value = IntVar()
-velocity = IntVar()
+value_up = IntVar()
+value_down = IntVar()
+value_right = IntVar()
+value_left = IntVar()
+value_close = IntVar()
+value_far = IntVar()
 
 image_up_open = Image.open('up.png')
 image_up = ImageTk.PhotoImage(image_up_open)
@@ -94,22 +87,27 @@ image_close = ImageTk.PhotoImage(image_close_open)
 image_far_open = Image.open('far.png')
 image_far = ImageTk.PhotoImage(image_far_open)
 
-Button(image=image_up, command=up).place(x=107, y=10)
-Button(image=image_down, command=down).place(x=107, y=162)
-Button(image=image_left, command=left).place(x=15, y=85)
-Button(image=image_right, command=right).place(x=200, y=85)
-Button(image=image_close, command=close).place(x=15, y=162)
-Button(image=image_far, command=far).place(x=200, y=10)
-Button(text='RUN', command=run).place(x=320, y=10)
-#Entry(textvariable=value, width=7).place(x=127, y=100)
-entry_velocity = Entry(textvariable=velocity, width=7)
-entry_velocity.insert(0, 50)
-entry_velocity.place(x=127, y=124)
+image_run_open = Image.open('run.png')
+image_run = ImageTk.PhotoImage(image_run_open)
+
+Button(image=image_run, text='RUN', command=run).place(x=270, y=10)
+Button(text='PROG', command=prog).place(x=270, y=100)
+Button(text='CLR', command=clr).place(x=270, y=170)
+Button(image=image_up, command=up).place(x=10, y=10)
+Button(image=image_down, command=down).place(x=10, y=80)
+Button(image=image_right, command=right).place(x=10, y=150)
+Button(image=image_left, command=left).place(x=10, y=220)
+Button(image=image_close, command=close).place(x=10, y=290)
+Button(image=image_far, command=far).place(x=10, y=360)
+Entry(textvariable=value_up, width=7).place(x=100, y=40)
+Entry(textvariable=value_down, width=7).place(x=100, y=110)
+Entry(textvariable=value_right, width=7).place(x=100, y=180)
+Entry(textvariable=value_left, width=7).place(x=100, y=260)
+Entry(textvariable=value_close, width=7).place(x=100, y=330)
+Entry(textvariable=value_far, width=7).place(x=100, y=400)
 
 
 while True:
-    if keyboard.is_pressed('space'):
-        send(mes)
     if keyboard.is_pressed('esc'):
         break    
     root.update()
